f69ba2f175a52a2f125aec6c39f36996f81efe11
Martin Nestorov <mnestorov@protonmail.com>
2020-03-04 21:12:18 +0200

    Add saving of a file

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :repl-options {:init-ns sf-pipo.core}
   :main sf-pipo.core
   :profiles {:dev
--- src/sf_pipo/core.clj
+++ src/sf_pipo/core.clj
@@ -2,10 +2,13 @@
   (:require [ring.adapter.jetty :as webserver]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.defaults :refer :all]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found]]
             [ring.handler.dump :refer [handle-dump]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.contrib.duck-streams :as ds]))
 
 (defn welcome
   "A warm welcome."
@@ -19,21 +22,22 @@
   [request]
   (let [filename (get-in request [:route-params :filename])]
     {:status 200
-     :body (str "I have to give you a file by this name " filename) ;;(slurp "justafile")
-     :headers {}
-     :content-type :txt}))
+     :body (io/input-stream "testfile")
+     :headers {}}))
 
 (defn save-file
   "Save a passed file to the fs."
   [request]
-  ;; TODO implement saving the file
-  )
+  (let [fi (get-in request [:multipart-params "file" :tempfile])]
+    (with-open [w (io/writer fi)]
+      (.write w "")))) ;; TODO save the file locally, not in `/tmp/`
 
 (defroutes app
   (GET "/" [] welcome)
   (GET "/file/:filename" [] get-file)
   (GET "/request-info" [] handle-dump)
-  (POST "/save/:filename" [] save-file)
+  (wrap-multipart-params
+   (POST "/save/:filename" [] save-file))
   (not-found "<h1>This is not the page you are looking for</h1>
               <p>Sorry, the page you requested was not found!</p>"))
 

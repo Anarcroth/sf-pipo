811a1c735c2c26eac9a86220d32666400580c477
Martin Nestorov <mnestorov@protonmail.com>
2020-05-07 22:49:44 +0300

    Add basic buttons for frontend

--- src/sfpipo/frontend.clj
+++ src/sfpipo/frontend.clj
@@ -6,6 +6,21 @@
   [request]
   (page/html5 {:lang "en"}
          [:body
-          [:div [:h1 "Sfpipo"]
-           (form/label "File Input" "File Input")
-           (form/file-upload "Upload")]]))
+          [:div [:h1 "Sfpipo"]]
+          [:dev
+           (form/form-to [:get "/ping"]
+                         (form/submit-button "Ping"))]
+          [:p]
+          [:dev
+           (form/form-to [:get "/list-files"]
+                         (form/submit-button "List files"))]
+          [:p]
+          [:dev
+           (form/text-field {:ng-model "fileName" :placeholder "Enter a file name here"} "file-name")
+           (form/form-to [:get "/file/fileName"]
+                         (form/submit-button "Get File"))]
+          [:p]
+          [:div
+           (form/text-field {:ng-model "userName" :placeholder "Enter a user name here"} "user-name")
+           (form/form-to [:get "/usr/userName"]
+                         (form/submit-button "Get User"))]]))

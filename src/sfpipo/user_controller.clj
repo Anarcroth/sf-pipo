9da5762737def848aac6584c0b6849d7ff5c3be9
Martin Nestorov <mnestorov@protonmail.com>
2020-08-27 21:38:58 +0300

    Fix general user deletion procedure

--- src/sfpipo/user_controller.clj
+++ src/sfpipo/user_controller.clj
@@ -16,9 +16,11 @@
 (defn delete-user
   [user-name]
   (log/info (format "Deleting user '%s'" user-name))
-  (if (= (db/delete-usr user-name) 1)
-    (format "Deleted user '%s'!" user-name)
-    (no-such-user user-name)))
+  (if (db/get-usr user-name)
+    (do
+      (db/delete-usr user-name)
+      (format "Deleted user '%s'" user-name))
+    (format "No such user '%s' exists!" user-name)))
 
 (defn create-user
   [name pass]

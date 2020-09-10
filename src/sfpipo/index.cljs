725a962882301cde9335c83c77bd1cd498f27c01
Martin Nestorov <mnestorov@protonmail.com>
2020-09-10 22:06:59 +0300

    Refactor how the lists are generated

--- src/sfpipo/index.cljs
+++ src/sfpipo/index.cljs
@@ -3,25 +3,31 @@
 
 (enable-console-print!)
 
+(def controller-links
+  [{:endpoint "/ping" :name "ping"}
+   {:endpoint "/list-users" :name "list-users"}
+   {:endpoint "/list-files" :name "list-files"}])
+
+(defn create-controller-links
+  [links]
+  (map (fn [link] [:p>a {:href (:endpoint link)} (:name link)]) links))
+
 (defn get-lists
   "Get the default 3 functionalities for the current time being."
   []
-  [:div>li
-   [:p [:a {:href "/ping"} "ping"]]
-   [:p [:a {:href "/list-users"} "list-users"]]
-   [:p [:a {:href "/list-files"} "list-files"]]])
+  [:ul (create-controller-links controller-links)])
 
 (def click-count (r/atom 0))
 
 (defn butonclick []
   [:div
-    "The atom " [:code "click-count"] " has value: "
-    @click-count ". "
-    [:input {:type "button" :value "Click me!"
-             :on-click #(swap! click-count inc)}]
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:input {:type "button" :value "Click me!"
+            :on-click #(swap! click-count inc)}]
    [get-lists]])
 
 (defn init-lists []
   (r/render-component
-   [butonclick]
+   [get-lists]
    (.getElementById js/document "sfpipo-main")))

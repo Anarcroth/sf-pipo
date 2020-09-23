d512d907467e43bd4dda0c9d537ff65a418dad6d
Martin Nestorov <mnestorov@protonmail.com>
2020-09-23 20:10:29 +0300

    Add db support for additional table columns

--- src/sfpipo/db.clj
+++ src/sfpipo/db.clj
@@ -51,34 +51,93 @@
   [name file]
   (sql/insert! sfpipo-db :enfile
                {:name name
-                :file (Files/readAllBytes (.toPath file))}))
+                :file (Files/readAllBytes (.toPath file))
+                :size (Files/size (.toPath file))}))
 
 (defn delete-file
+  "Delete a file object from the `enfile` table with the given `name`."
   [name]
   (sql/delete! sfpipo-db :enfile ["name = ?" name]))
 
-(defn get-file-objects []
+(defn get-file-objects
+  "Gather all `enfile` file objects from the `enfile` table."
+  []
   (sql/query sfpipo-db ["select * from enfile"]))
 
-(defn get-whole-files []
+(defn get-file-by-id
+  "Get file object by `file-id` from `enfile` table."
+  [file-id]
+  (sql/query sfpipo-db ["select * from enfile where id = ?" file-id]))
+
+(defn get-whole-files
+  "Get only the files as bytes from the `enfile` table."
+  []
   (map :file (sql/query sfpipo-db ["select file from enfile"])))
 
-(defn get-file-names []
+(defn get-whole-file-by-id
+  "Get only the file as bytes by `file-id` from the `enfile` table."
+  [file-id]
+  (:file (sql/query sfpipo-db ["select file from enfile where id = ?" file-id])))
+
+(defn get-file-names
+  "Get only the file names from the `enfile` table."
+  []
   (map :name (sql/query sfpipo-db ["select name from enfile"])))
 
+(defn get-file-name-by-id
+  "Get only the file name by `file-id` from the `enfile` table."
+  [file-id]
+  (:name (sql/query sfpipo-db ["select name from enfile where id = ?" file-id])))
+
+(defn get-file-sizes
+  "Get only the file sizes from the `enfile` table."
+  []
+  (map :size (sql/query sfpipo-db ["select size from enfile"])))
+
+(defn get-file-size-by-id
+  "Get only the file size by `file-id` from the `enfile` table."
+  [file-id]
+  (:size (sql/query sfpipo-db ["select size from enfile where id = ?" file-id])))
+
+(defn get-file-downloadable-links
+  "Get only the downloadable links from the `enfile` table."
+  []
+  (map :downloadable_link (sql/query sfpipo-db ["select downloadable_link from enfile"])))
+
+(defn get-file-downloadable-link
+  "Get only the downloadable link for a file."
+  [file-id]
+  (:downloadable_link (sql/query sfpipo-db ["select downloadable_link from enfile where id = ?" file-id])))
+
 (defn get-file-name
+  "`Deprecated`. Kept for backwards compatibility."
   [filename]
-  (:name (sql/query sfpipo-db ["select * from enfile where name = ?" filename]
+  (:name (sql/query sfpipo-db
+                    ["select * from enfile where name = ?" filename]
+                    {:result-set-fn first})))
+
+(defn get-files
+  "Get same named files by `filename` from the `enfile` table."
+  [filename]
+  (:file (sql/query sfpipo-db
+                    ["select * from enfile where name = ?" filename]
                     {:result-set-fn first})))
 
 (defn get-file
   [filename]
-  (:file (sql/query sfpipo-db ["select * from enfile where name = ?" filename]
+  (:file (sql/query sfpipo-db
+                    ["select * from enfile where name = ?" filename]
                     {:result-set-fn first})))
 
 (defn delete-usr
+  "`Deprecated`. Kept for backwards compatibility."
   [username]
   (sql/delete! sfpipo-db :users ["name = ?" username]))
+
+(defn delete-usr-by-id
+  "Delete user by `user-id` from the `users` table."
+  [user-id]
+  (sql/delete! sfpipo-db :users ["id = ?" user-id]))
 
 (defn insert-usr
   [username password]
@@ -90,11 +149,18 @@
   []
   (sql/query sfpipo-db ["select * from users"]))
 
+(defn get-user-by-id
+  "Get user by `user-id` from the `users` table."
+  [user-id]
+  (sql/query sfpipo-db ["select * from users where id = ?" user-id]))
+
 (defn get-all-usernames
   []
   (map str (map :name (get-all-users)) "\n"))
 
 (defn get-usr
+  "`Deprecated`. Kept for backwards compatibility."
   [username]
-  (sql/query sfpipo-db ["select * from users where name = ?" username]
+  (sql/query sfpipo-db
+             ["select * from users where name = ?" username]
              {:result-set-fn first}))

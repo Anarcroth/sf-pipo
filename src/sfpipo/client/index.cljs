c5252e62b4cf6009817ecfcc848827b4f6975f35
Martin Nestorov <mnestorov@protonmail.com>
2020-09-24 17:28:33 +0300

    Refactor cljs client code

--- src/sfpipo/client/index.cljs
+++ src/sfpipo/client/index.cljs
@@ -6,36 +6,37 @@
 
 (enable-console-print!)
 
-(def controller-links
+(defonce controller-links
   [{:endpoint "/ping" :name "ping"}
    {:endpoint "/list-users" :name "list-users"}
    {:endpoint "/list-files" :name "list-files"}])
-
 (defonce pong-res (r/atom ""))
-
 (defonce file-upload-res (r/atom ""))
+(defonce file-name-input (r/atom ""))
 
-(defn create-controller-links
+(defn- create-controller-links
+  "Creates link tags for each endpoint defined in `controller-links`."
   [links]
   (map (fn [link] [:p>a {:href (:endpoint link)} (:name link)]) links))
 
-(defn static-ping
+(defn- call-ping
   []
   (go (let [response (<! (http/get "/ping"))]
         (= (:status response) 200))))
 
-(defn get-file [name]
-  (go (let [response (<! (http/get (str "/file/" name)))]
-        (prn response))))
-
-(defn handle-ping-press []
-  (if (static-ping)
-      #(swap! pong-res (fn [] (str "Pong! Service is up :)")))))
+(defn- handle-ping-press []
+  (if (call-ping)
+    #(swap! pong-res (fn [] (str "Pong!")))
+    #(swap! pong-res (fn [] (str "Service down!")))))
 
 (defn ping-button []
   [:div {:id "ping-button"}
    [:input {:type "button" :value "Ping" :class ["button"]
             :on-click (handle-ping-press)}]])
+
+(defn- get-file [name]
+  (go (let [response (<! (http/get (str "/file/" name)))]
+        (prn response))))
 
 (defn download-button []
   [:div {:id "download-button"}
@@ -43,58 +44,60 @@
     [:input {:type "text"}]
     [:input {:type "submit" :value "Download file"}]]])
 
-(defn handle-file-upload [f]
-  (if (save-file-to-db f)
-    #(swap! file-upload-res (fn [] (str "File uploaded!")))))
-
-(defn save-file-to-db [f]
+(defn- save-file-to-db [f]
   (go (let [response (<! (http/post "/upload" {:multipart-params [["file" f]]}))]
         (= (:status response) 200))))
 
-(defn file-input [on-result]
+(defn- handle-file-upload [f]
+  (if (save-file-to-db f)
+    #(swap! file-upload-res (fn [] (str "File uploaded!")))
+    #(swap! file-upload-res (fn [] (str "File upload failed!")))))
+
+(defn file-input []
   [:input {:type "file"
            :on-change
            (fn [this]
              (if (not (= "" (-> this .-target .-value)))
                (let [^js/File file (-> this .-target .-files (aget 0))]
-                 (save-file-to-db file)
+                 (handle-file-upload file)
                  (set! (-> this .-target .-value) ""))))}])
 
 (defn- parse-file-res [res]
   (as-> res r
-      (:body r)
-      (.parse js/JSON r)
-      (js->clj r :keywordize-keys true)))
-
-(def file-name-input (r/atom ""))
+    (:body r)
+    (.parse js/JSON r)
+    (js->clj r :keywordize-keys true)))
 
 (defn- create-file-table [f]
   (r/render-component
-   [:table>tbody
-    [:tr
-     [:th "Id"]
-     [:th "Name"]
-     [:th "Size"]]
-    [:tr
-     [:td (:id f)]
-     [:td (:name f)]
-     [:td (:size f)]
-     ]]
+   [:div
+    [:p]
+    [:table>tbody
+     [:tr
+      [:th "Id"]
+      [:th "Name"]
+      [:th "Size"]]
+     [:tr
+      [:td (:id f)]
+      [:td (:name f)]
+      [:td (:size f)]
+      ]]]
    (.getElementById js/document "file-table")))
 
-(defn get-file-from-db [file-name]
+(defn get-file-from-db
   "Get file from db and output file name, size, link, etc"
+  [file-name]
   (go (let [response (<! (http/get (str "/file/" file-name)))]
         (if (= (:status response) 200)
           (create-file-table (parse-file-res response))))))
 
-(defn form []
+(defn file-search-form []
   [:div
    [:input {:type "text"
             :value @file-name-input
             :on-change #(reset! file-name-input (.-value (.-target %)))
             }]
-   [:button  {:on-click #(get-file-from-db @file-name-input)} "Type file name"]])
+   [:button  {:on-click #(get-file-from-db @file-name-input)} "Search for file"]])
 
 (defn get-lists
   "Get the default 3 functionalities for the current time being."
@@ -106,7 +109,7 @@
    [ping-button]
    [:p @file-upload-res]
    [file-input]
-   [form]])
+   [file-search-form]])
 
 (defn init-lists []
   (r/render-component
